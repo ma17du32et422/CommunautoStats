0025668a02a651edd2f824c2a526bc29b60d2bb0
Charles <lesagecharles.secondaire@gmail.com>
2025-06-02 12:32:46 -0400

    liste véhicules ajoutée

--- main.mjs
+++ main.mjs
@@ -38,14 +38,39 @@ while(true) {
     const carsSorted = cars.sort((a, b) => a.number - b.number);
     const file = fs.writeFileSync('vehicles.json', JSON.stringify(carsSorted, null, 2));
 
+    const fileContent = fs.readFileSync('listeVehicules.json', 'utf-8');
+    const listeVehicules = JSON.parse(fileContent);
 
-    /*const replacer = (key, value) => value === null ? '' : value
-    const header = Object.keys(cars[0])
-    const csv = [
-        header.join(','), // header row first
-        ...cars.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
-    ].join('\r\n')
-    */
+// Ajouter les véhicules manquants à la liste
+    for (let i = 0; i < carsSorted.length; i++) {
+        const alreadyExists = listeVehicules.some(v => v.number === carsSorted[i].number);
+        if (!alreadyExists) {
+            listeVehicules.push(carsSorted[i]);
+        }
+    }
+    const file2 = fs.writeFileSync('listeVehicules.json', JSON.stringify(listeVehicules.sort((a, b) => a.number - b.number), null, 2));
+
+    const worksheet2 = XLSX.utils.json_to_sheet(listeVehicules);
+    const listePath = "./data/listeVehicules.xlsx";
+    if(fs.existsSync(listePath)){
+        const workbook = XLSX.readFile(listePath);
+        if (workbook.SheetNames.includes("liste")) {
+            delete workbook.Sheets["liste"];
+            const index = workbook.SheetNames.indexOf("liste");
+            if (index !== -1) {
+                workbook.SheetNames.splice(index, 1);
+            }
+        }
+        XLSX.utils.book_append_sheet(workbook, worksheet2, "liste");
+        XLSX.writeFile(workbook, listePath);
+        console.log("Liste actualisée!");
+    } else {
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet2, "liste");
+        XLSX.writeFile(workbook, listePath);
+        console.log("nouvelle liste créée");
+    }
+
 
     const path = "./data/FLEXs.xlsx";
     const worksheet = XLSX.utils.json_to_sheet(cars);
@@ -65,6 +90,10 @@ while(true) {
         XLSX.writeFile(workbook, path);
         console.log("nouveau fichier créé et données inscrites");
     }
+
+    const newPath = "./data/FLEXs_lecture.xlsx";
+    fs.copyFileSync(path, newPath);
+
     const timeout = parseInt(values.delay)*1000*60;
     await wait(timeout);
     //process.exit();
